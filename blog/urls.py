a01614999d4f5a82e7c0393e76da9f69a62b412d
lucas <lmontesinos10@gmail.com>
2017-08-09 22:56:14 +0200

    personal website

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='index'),
+    url('map/$', views.map, name='map'),
+    url('exp/$', views.exp, name='exp'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 def post_list(request):
     return render(request, 'blog/index.html', {})
+
+def map(request):
+    return render(request, 'blog/map.html', {})
+
+def index(request):
+    return render(request, 'blog/index.html', {})
+
+def exp(request):
+    return render(request, 'blog/exp.html', {})
